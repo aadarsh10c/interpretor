d1135ee38e0683c9c2914d9b9de47fe872e0de6f
aadarsh010c <adarsh010c@gmail.com>
2024-03-22 20:11:33 +0530

    Added test for int

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -36,12 +36,34 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+func (l *Lexer) readNumber() string{
+	position := l.position
+	for isDigit(l.ch){
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+func (l *Lexer) skipWhitespace(){
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r'{
+		l.readChar()
+	}
+}
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+//returns  the new token each time it is called  
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	//skip unncessary whitespace charachters
+	l.skipWhitespace()
+
+	//set the token value based upon the current charahcter or group of charachter
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -67,10 +89,15 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
-		} else {
+		}else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		}else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
 	}
+	//read the next charahcter after setting the token
 	l.readChar()
 	return tok
 }
